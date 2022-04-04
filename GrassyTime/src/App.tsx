83a44b443d1508a4037a0bc65e7661749fff432a
Lennie De Villiers <lenniedg@gmail.com>
2022-04-04 11:09:55 +0200

    .

--- GrassyTime/src/App.tsx
+++ GrassyTime/src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import {Dimensions, StyleSheet, Text, View} from 'react-native';
-
-const {height} = Dimensions.get('screen');
+import React from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import { Button } from "@rneui/themed";
+import Cards from "./components/Cards";
+const { height } = Dimensions.get("screen");
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.center}>
-        <Text>Hello React Native Web!!!</Text>
-      </View>
+      <Button
+              title={"React Native Elements"}
+              containerStyle={{
+                width: 200,
+                marginHorizontal: 50,
+                marginVertical: 10,
+              }}
+            />
+            <Cards/>
     </View>
   );
 };
