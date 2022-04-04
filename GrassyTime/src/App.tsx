d254df8c7de4626a29dda1f78cf39ad45a18745f
Lennie De Villiers <lenniedg@gmail.com>
2022-04-04 14:44:35 +0200

    Add Firebase auth

--- GrassyTime/src/App.tsx
+++ GrassyTime/src/App.tsx
@@ -1,34 +1,64 @@
-import React from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text } from "react-native";
 import { Button } from "@rneui/themed";
-import Cards from "./components/Cards";
-const { height } = Dimensions.get("screen");
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Button
-              title={"React Native Elements"}
-              containerStyle={{
-                width: 200,
-                marginHorizontal: 50,
-                marginVertical: 10,
+function App() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState<boolean>(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  function doLogin() {
+    auth()
+  .signInWithEmailAndPassword('lennie.work@gmail.com', '5/.Ewt~U=(Ty4S2<')
+  .then(() => {
+    console.log('User account created & signed in!');
+  })
+  .catch(error => {
+    if (error.code === 'auth/email-already-in-use') {
+      console.log('That email address is already in use!');
+    }
+
+    if (error.code === 'auth/invalid-email') {
+      console.log('That email address is invalid!');
+    }
+
+    console.error(error);
+  });
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+          <Button
+              title="Login"
+              onPress={() => {
+                doLogin();
               }}
             />
-            <Cards/>
+      </View>
+    );
+  }
+
+  return (
+    <View>
+      <Text>Welcome {user.email}</Text>
     </View>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    height,
-  },
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+}
 
 export default App;
