b8552af591573373990e66a47e9e1a49b29cdb5e
Lennie De Villiers <lenniedg@gmail.com>
2022-04-11 18:40:01 +0200

    Formatting

---
+++ GrassyTime/src/components/GrassType/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { IGrassType } from '../../constants/grass';
+import styles from './styles';
+
+interface IGrassTypeView {
+    details: IGrassType | undefined;
+}
+
+const GrassTypeView = (props: IGrassTypeView) => {
+    const { type, summer, winter, mow_length } = props?.details || {};
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.line}>Type: {type}</Text>
+            <Text style={styles.line}>Growth Rate Per Day In Summer: {summer} cm</Text>
+            <Text style={styles.line}>Growth Rate Per Day In Winter: {winter} cm</Text>
+            <Text style={styles.line}>Best Length to Mow: {mow_length} cm</Text>
+        </View>
+    )
+}
+
+export default GrassTypeView;
